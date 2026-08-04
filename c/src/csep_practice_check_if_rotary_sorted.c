e7a581d22e04181acc6568771c67b84a6320908f
kartikey singh chauhan <kartikeysinghchauhan669@gmail.com>
2026-08-05 00:26:03 +0530

    added functionality to check if a rotated array is sorted or not

---
+++ c/src/csep_practice_check_if_rotary_sorted.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isSortedRotated(int arr[], int n) {
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > arr[(i + 1) % n]) {
+            count++;
+        }
+    }
+
+    return count <= 1;
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    if (isSortedRotated(arr, n))
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
