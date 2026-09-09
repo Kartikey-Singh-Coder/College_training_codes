179da6599bb7504560aa581c8acee5f870c39d33
kartikey singh chauhan <kartikeysinghchauhan669@gmail.com>
2026-09-09 14:00:46 +0530

    ADD: reversal,insertion(arbitrary index) and deletion (arbitrary index)

--- cpp/src/csep_practice_linked_list.cpp
+++ cpp/src/csep_practice_linked_list.cpp
@@ -3,8 +3,8 @@ using namespace std;
 typedef struct node {
     int data;
     node *next;
-}node;
-node* createnode() {
+}snode;
+static node* createnode() {
     node* newnode = new node;
     int data = 0;
     printf("enter data for this node : ");
@@ -13,7 +13,7 @@ node* createnode() {
     newnode->next = nullptr;
     return newnode;
 }
-node* createlist(int nodelimit,node* head) {
+static node* createlist(int nodelimit,node* head) {
     for (int i = 0; i < nodelimit; i++) {
         node* newnode = createnode();
         if (head == nullptr) {
@@ -30,7 +30,7 @@ node* createlist(int nodelimit,node* head) {
     }
     return head;
 }
-void displaylist(node* head) {
+static void displaylist(node* head) {
     node* temp = head;
     while (temp != nullptr) {
         cout << temp->data << "->";
@@ -38,11 +38,109 @@ void displaylist(node* head) {
     }
     cout << "null" << endl;
 }
+static node* middle(node* head) {
+    node* slow = head;
+    node* fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+void static printnode(const node* node) {
+    cout <<endl<< "null->" << node->data << "<-null" << endl;
+    cout << endl;
+}
+bool static L_Search(node* head) {
+    int data = 0;
+    cout << "enter element : " << endl;
+    cin>>data;
+    node* temp = head;
+    while (temp != nullptr) {
+        if (temp->data == data) {
+            cout <<"entry found"<<endl;
+            return true;
+        }
+        temp= temp->next;
+    }
+    return false;
+}
+static node* LL_Reversal(node*& head) {
+    node* temp = head;
+    node* prev = nullptr;
+    while (temp != nullptr) {
+        node* current = temp;
+        temp = temp->next;
+        current->next = prev;
+        prev = current;
+    }
+    return prev;
+}
+void delete_at_pos(node* &head) {
+    node* temp = head;
+    node* current = head;
+    cout<<  "enter the position of element you want to delete"<<endl;
+    int pos = 0;
+    cin >> pos;
+    if (pos == 0) {
+        head = head->next;
+        delete current;
+        return;
+    }
+    while (pos > 2 && temp != nullptr) {
+        temp = temp->next;
+        pos--;
+    }
+    if (temp == nullptr) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    current = temp->next;
+    temp->next = current->next;
+    delete current;
+}
+void insert_at_pos(node*& head) {
+    int pos;
+    cout << "enter the position of element" << endl;
+    cin >> pos;
+    int data = 0;
+    cout << "enter the data" << endl;
+    cin >> data;
+    node* newnode = new node;
+    newnode->data = data;
+    if (pos == 0) {
+        newnode->next = head;
+        head = newnode;
+        return;
+    }
+    int count = pos;
+    node* temp = head;
+    while (count > 2 && temp != nullptr) {
+        temp = temp->next;
+        count--;
+    }
+    if (temp == nullptr) {
+        cout << "Invalid position" << endl;
+        delete newnode;
+        return;
+    }
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
 int main() {
     node* head = nullptr;
     int nodes = 0;
     cout<< "enter number of linked list nodes: "<<endl;
     cin>> nodes;
-    head = createlist(nodes,head);
+    head = createlist(nodes,head);            // This part creates the linked list to the head pointer
+    displaylist(head);                        // This part displays the complete linked list
+    const node* middlenode = middle(head);    // This part finds the middle node
+    printnode(middlenode);                    // This part prints the middle node data
+    cout<<L_Search(head)<<endl;               // This part linearly searches the list
+    head = LL_Reversal(head);             // this part reverses a linked list
+    insert_at_pos(head);                  // This part inserts an element at an arbitrary index
     displaylist(head);
+    delete_at_pos(head);                  // This part deletes an element at any arbitrary index
+    displaylist(head);
+    return 0;
 }
